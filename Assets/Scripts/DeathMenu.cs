64a2dcd3c0a9263a66e374d0af45d2349e9ddbd6
Taylor Livingston <ttl1007@westminstercollege.edu>
2018-04-12 20:11:28 -0600

    Score update

--- Assets/Scripts/DeathMenu.cs
+++ Assets/Scripts/DeathMenu.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Mono.Data.SqliteClient;
 using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour {
 
     public Text scoreText;
+	public Text tokenText;
+
     public Image backgroundImage;
     private bool isShowed = false;
     private float transition = 0.0f;
@@ -32,11 +37,73 @@ public class DeathMenu : MonoBehaviour {
     }
 
     public void Restart() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+		SceneManager.LoadScene("Menu");
+		//direct db connection to where the db is stored in app
+		//and open connection
+		const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
+		IDbConnection dbcon = new SqliteConnection(connectionString);
+		dbcon.Open();
+		IDbCommand dbcmd = dbcon.CreateCommand();
 
-    public void ToMenu() {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
-    }
+		//create query for adding score
+		//dbcmd = null;
+		dbcon.CreateCommand();
+		const string command =
+		"INSERT INTO Score " +
+		"(userID, totalScore, balance, grade) " +
+		"VALUES (@two, @three, @four, @five)";
+
+		//dbcmd.Parameters.Add(new SqliteParameter("@one", one)); 
+		dbcmd.Parameters.Add(new SqliteParameter("@two", SubmitName.getStuID()));
+		dbcmd.Parameters.Add(new SqliteParameter("@three", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@four", Int32.Parse(tokenText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@five", SubmitName.getStuGrade()));
+
+		string sql = command;
+		Debug.Log(sql);
+		Debug.Log(SubmitName.getTeachID());
+		Debug.Log(scoreText.text);
+		Debug.Log (tokenText.text);
+		Debug.Log(SubmitName.getStuGrade());
+
+		dbcmd.CommandText = command;
+		IDataReader reader = dbcmd.ExecuteReader();
+
+	}
+
+	public void ToMenu() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Menu");
+		//direct db connection to where the db is stored in app
+		//and open connection
+		const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
+		IDbConnection dbcon = new SqliteConnection(connectionString);
+		dbcon.Open();
+		IDbCommand dbcmd = dbcon.CreateCommand();
+
+		//create query for adding score
+		dbcmd = null;
+		dbcon.CreateCommand();
+		const string command =
+			"INSERT INTO Score " +
+			"(userID, totalScore, balance, grade) " +
+			"VALUES (@two, @three, @four, @five)";
+
+		//dbcmd.Parameters.Add(new SqliteParameter("@one", one)); 
+		dbcmd.Parameters.Add(new SqliteParameter("@two", SubmitName.getStuID()));
+		dbcmd.Parameters.Add(new SqliteParameter("@three", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@four", Int32.Parse(scoreText.text)));
+		dbcmd.Parameters.Add(new SqliteParameter("@five", SubmitName.getStuGrade()));
+
+		string sql = command;
+		Debug.Log(sql);
+		Debug.Log(SubmitName.getTeachID());
+		Debug.Log(scoreText.text);
+		Debug.Log (tokenText.text);
+		Debug.Log(SubmitName.getStuGrade());
+		dbcmd.CommandText = command;
+		IDataReader reader = dbcmd.ExecuteReader();
+
+	} 
+		
 }
--- Assets/Scripts/SubmitName.cs
+++ Assets/Scripts/SubmitName.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
@@ -8,130 +9,153 @@ using Mono.Data.SqliteClient;
 using UnityEngine.SceneManagement;
 
 public class SubmitName : MonoBehaviour
-	{
+{
 	//create array lists for the DB
 	private ArrayList userList = new ArrayList();
 	private ArrayList teacherList = new ArrayList();
 	private static int currTeachID = 0;
 	private static int currStuID = 0;
+	private static string stuGrade;
 
 
 	public Button LoginButton;
-		void Start ()
+	void Start ()
+	{
+		var input = GameObject.Find("InputField").GetComponent<InputField>();
+		var se= new InputField.SubmitEvent();
+		se.AddListener(CheckName);
+		input.onEndEdit = se;
+
+		//or simply use the line below, 
+		//input.onEndEdit.AddListener(CheckName);  // This also works
+
+
+	}
+
+	public static int getTeachID()
+	{
+		return currTeachID;
+	}
+	public static int getStuID()
+	{
+		return currStuID;
+	}
+	public static String getStuGrade()
+	{
+		return stuGrade;
+	}
+
+	private void CheckName(string arg0)
+	{
+
+
+		//direct db connection to where the db is stored in app
+		//and open connection
+		const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
+		IDbConnection dbcon = new SqliteConnection (connectionString);
+		dbcon.Open ();
+
+		//create query for user name
+		IDbCommand dbcmd = dbcon.CreateCommand ();
+		const string sql =
+			"SELECT StuUserName " +
+			"FROM student";
+		dbcmd.CommandText = sql;
+		IDataReader reader = dbcmd.ExecuteReader ();
+
+		while (reader.Read()) 
 		{
-			var input = GameObject.Find("InputField").GetComponent<InputField>();
-			var se= new InputField.SubmitEvent();
-			se.AddListener(CheckName);
-			input.onEndEdit = se;
+			string user = reader.GetString(0);
 
-			//or simply use the line below, 
-			//input.onEndEdit.AddListener(CheckName);  // This also works
+			Debug.Log(user);
 
+			userList.Add(user);
 
 		}
+		//create query for teacher login
+		dbcmd = dbcon.CreateCommand ();
+		const string sql2 =
+			"SELECT TeachUserName " +
+			"FROM teacher";
+		dbcmd.CommandText = sql2;
+		reader = dbcmd.ExecuteReader ();
 
-		public static int getTeachID()
+		while (reader.Read()) 
 		{
-			return currTeachID;
-		}
-		public static int getStuID()
-		{
-			return currStuID;
-		}
+			string teacher = reader.GetString(0);
 
-		private void CheckName(string arg0)
+			Debug.Log(teacher);
+
+			teacherList.Add(teacher);
+
+		}
+		//if user is in DB store their userID for later use and allow them to login 
+		if (userList.Contains (arg0)) {
+			dbcmd = dbcon.CreateCommand();
+			const string sql4 =
+				"SELECT StuID " +
+				"FROM student " +
+				"WHERE StuUserName = @arg0";
+
+			dbcmd.Parameters.Add(new SqliteParameter("@arg0", arg0));
+			dbcmd.CommandText = sql4;
+			reader = dbcmd.ExecuteReader();
+			while (reader.Read())
 			{
+				int stuID = Int32.Parse(reader.GetString(0));
 
+				Debug.Log(currStuID);
 
-				//direct db connection to where the db is stored in app
-				//and open connection
-				const string connectionString = "URI=file:Assets\\Plugins\\MumboJumbos.db";
-				IDbConnection dbcon = new SqliteConnection (connectionString);
-				dbcon.Open ();
-
-				//create query for user name
-				IDbCommand dbcmd = dbcon.CreateCommand ();
-				const string sql =
-					"SELECT StuUserName " +
-					"FROM student";
-				dbcmd.CommandText = sql;
-				IDataReader reader = dbcmd.ExecuteReader ();
-
-				while (reader.Read()) 
-				{
-					string user = reader.GetString(0);
-
-					Debug.Log(user);
-
-					userList.Add(user);
-
-				}
-				//create query for teacher login
-				dbcmd = dbcon.CreateCommand ();
-				const string sql2 =
-					"SELECT TeachUserName " +
-					"FROM teacher";
-				dbcmd.CommandText = sql2;
-				reader = dbcmd.ExecuteReader ();
-
-				while (reader.Read()) 
-				{
-					string teacher = reader.GetString(0);
-
-					Debug.Log(teacher);
-
-					teacherList.Add(teacher);
-
-				}
-			    //if user is in DB store their userID for later use and allow them to login 
-				if (userList.Contains (arg0)) {
-				dbcmd = dbcon.CreateCommand();
-				const string sql4 =
-					"SELECT StuID " +
-					"FROM student " +
-					"WHERE StuUserName = @arg0";
-
-				dbcmd.Parameters.Add(new SqliteParameter("@arg0", arg0));
-				dbcmd.CommandText = sql4;
-				reader = dbcmd.ExecuteReader();
-				while (reader.Read())
-				{
-					int stuID = Int32.Parse(reader.GetString(0));
-
-					Debug.Log(currStuID);
-
-					currStuID = stuID;
-
-				}
-				LoginButton.interactable = true;
-                //if teacher username found, store teacherID for later use and take them to the teacher menu
-				} else if (teacherList.Contains (arg0)) {
-					dbcmd = dbcon.CreateCommand();
-					const string sql3 =
-						"SELECT TeachID " +
-						"FROM teacher " +
-						"WHERE TeachUserName = @arg0";
-
-					dbcmd.Parameters.Add(new SqliteParameter("@arg0", arg0));
-					dbcmd.CommandText = sql3;
-					reader = dbcmd.ExecuteReader();
-					while (reader.Read())
-					{
-						int teachID = Int32.Parse(reader.GetString(0));
-
-						Debug.Log(currTeachID);
-
-						currTeachID = teachID;
-
-					}
-
-					SceneManager.LoadScene("TeacherMenu");
-				}
-				else {
-					SceneManager.LoadScene("Login");
-				}
-
+				currStuID = stuID;
 
 			}
-}
+			dbcmd = dbcon.CreateCommand();
+			const string sql5 =
+				"SELECT Grade " +
+				"FROM student " +
+				"WHERE StuUserName = @arg0";
 
+			dbcmd.Parameters.Add(new SqliteParameter("@arg0", arg0));
+			dbcmd.CommandText = sql4;
+			reader = dbcmd.ExecuteReader();
+			while (reader.Read())
+			{
+				string grade = reader.GetString(0);
+
+				Debug.Log(grade);
+
+				stuGrade = grade;
+
+			}
+			LoginButton.interactable = true;
+			//if teacher username found, store teacherID for later use and take them to the teacher menu
+		} else if (teacherList.Contains (arg0)) {
+			dbcmd = dbcon.CreateCommand();
+			const string sql3 =
+				"SELECT TeachID " +
+				"FROM teacher " +
+				"WHERE TeachUserName = @arg0";
+
+			dbcmd.Parameters.Add(new SqliteParameter("@arg0", arg0));
+			dbcmd.CommandText = sql3;
+			reader = dbcmd.ExecuteReader();
+			while (reader.Read())
+			{
+				int teachID = Int32.Parse(reader.GetString(0));
+
+				Debug.Log(currTeachID);
+
+				currTeachID = teachID;
+
+			}
+
+			SceneManager.LoadScene("TeacherMenu");
+		}
+		else {
+			SceneManager.LoadScene("Login");
+		}
+
+
+	}
+}
+	
