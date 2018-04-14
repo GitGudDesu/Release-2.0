6c922b4946337797ef65475f1ee5f23eb565d728
heytoshi <enkhbat_tsogt@yahoo.com>
2018-04-13 20:29:06 -0600

    "Track Token" has been added

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mono.Data.Sqlite;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,9 @@ using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
+
+    public static float coins = 0;
+    public static float tCoins = 0;
 
     private CharacterController controller;
     private float speed = 3.75f;
@@ -103,6 +107,10 @@ public class CharacterControl : MonoBehaviour
         if (hit.point.z > transform.position.z + 0.1f && hit.gameObject.tag == "collectible") {
             Destroy(hit.gameObject);
             ScoreforCoin.CoinScore++;
+            //coins = (float)FindData("student", "Coin", "StuID", SubmitName.getStuID());
+            tCoins = ScoreforCoin.CoinScore;
+            DBSaveLoad.UpdateData("student", "StuID", SubmitName.getStuID(), "Coin", ScoreforCoin.CoinScore);
+            
         }
 
             if (hit.point.z > transform.position.z + 0.1f && hit.gameObject.tag == "Coin") {
@@ -167,4 +175,35 @@ public class CharacterControl : MonoBehaviour
 		CoinPick.ResetVars ();
 
     }
+
+
+
+    public object FindData(string tableName, string selectHeader, string whereHeader, object whereValue)
+    {
+        using (var dbConnection = new SqliteConnection("URI=file:" + Application.dataPath + "/Plugins/MumboJumbos.db"))
+        {
+            dbConnection.Open();
+
+            using (var dbCommand = dbConnection.CreateCommand())
+            {
+                dbCommand.CommandText = string.Format("SELECT {0} " +
+                                                       "FROM {1} " +
+                                                       "WHERE {2} = @whereValue", selectHeader, tableName, whereHeader);
+
+                SqliteParameter whereParam = new SqliteParameter("@whereValue", whereValue);
+                dbCommand.Parameters.Add(whereParam);
+
+                using (var dbReader = dbCommand.ExecuteReader())
+                {
+                    while (dbReader.Read())
+                    {
+                        if (dbReader.GetValue(0) != null)
+                            return dbReader.GetValue(0);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
 }
